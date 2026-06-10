7024bc7ef7cd0e1d7947835c730d4b6ebc42ff9a
Alexandr-Rschin <rosinaleksandr183@gmail.com>
2026-06-10 09:53:16 +0300

    Update point.cpp

--- point.cpp
+++ point.cpp
@@ -1,23 +1,32 @@
+point.cpp
+
 #include "point.h"
 #include <cmath> 
+#include <limits> // Добавили для использования std::numeric_limits
+
 Point::Point(const double x, const double y)
 {
     this->x = x;
     this->y = y;
 }
+
 double Point::getX() const
 {
     return this->x;
 }
+
 double Point::getY() const
 {
     return this->y;
 }
+
 bool Point::operator==(const Point& p) const
 {
-    const double EPS = 1e-9;
+    // Использование встроенной константы машинного эпсилон
+    const double EPS = std::numeric_limits<double>::epsilon();
     return (std::abs(this->x - p.x) < EPS && std::abs(this->y - p.y) < EPS);
 }
+
 bool Point::operator!=(const Point& p) const
 {
     return !(*this == p);
